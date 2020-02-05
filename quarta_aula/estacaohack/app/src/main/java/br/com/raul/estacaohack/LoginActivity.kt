2d4c2d0d56f61e0be8993bcb0fab92e94d472864
wesleyclzns <wesclzns@gmail.com>
2020-02-05 17:04:55 -0300

    final da quarta aula

--- quarta_aula/estacaohack/app/src/main/java/br/com/raul/estacaohack/LoginActivity.kt
+++ quarta_aula/estacaohack/app/src/main/java/br/com/raul/estacaohack/LoginActivity.kt
@@ -2,11 +2,21 @@ package br.com.raul.estacaohack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //Criando o click do botão entrar
+        btnEntrar.setOnClickListener {
+
+            //recupendo os valores digitados
+            val usuario = edtUsuario.text.toString().trim()
+            val senha = edtSenhaLogin.text.toString().trim()
+        }
     }
 }
